9de29ebe2980d221c93a3f474dc9f6267663c7dd
Chloe ONeil <chloesporri@yahoo.com>
2022-10-31 19:51:49 -0700

    add window.onload with const and lets for age  Co-authored-by: Mike Wilkes <mikewilkes@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 window.onload = function () {
+  const form = document.querySelector("form");
+  form.onsubmit = function(event) {
+      event.preventDefault();
 
+      let legalAdult = document.getElementById("adult");
+      legalAdult.setAttribute("class", "hidden");
+      let minors = document.getElementById("minor");
+      minors.setAttribute("class", "hidden");
 
-
-};
+      const age = parseInt(document.querySelector("input#age").value);
+      
+      if (age >= 18) {
+        legalAdult.removeAttribute("class");
+      } else {
+        minors.removeAttribute("class");
+      }
+    };
+  };
